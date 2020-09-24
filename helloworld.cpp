16e93715f2a78166d1b09f1989b3db28621ee233
Valentin68 <valentin.mercy.vm@gmail.com>
2020-09-24 12:41:27 +0200

    Implemented hello world

---
+++ helloworld.cpp
@@ -0,0 +1,15 @@
+/**
+* @author Valentin Mercy
+* @file helloworld.cpp
+* @date 24/09/2020
+* @brief Just a simple helloworld program
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout << "Hello World";
+	return 0;
+}
