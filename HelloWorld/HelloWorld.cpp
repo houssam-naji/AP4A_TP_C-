8484f32c6678179c8d32bf385d884b0740104321
Folaste <leblancflo@orange.fr>
2020-09-27 22:45:23 +0200

    Prints HelloWorld!

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,16 @@
+/*
+ * @author Leblanc_Florian
+ * @file helloworld.cpp
+ * @date 26/09/2020
+ * @brief Prints "HelloWorld!"
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World !";
+	return 0;
+}
