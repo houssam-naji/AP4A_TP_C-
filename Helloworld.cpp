21cad07c2ba275e93cddedbc4e0437f5381d3472
Jeremy <Baodman@icloud.com>
2020-09-27 21:27:14 +0800

    helloworld

---
+++ Helloworld.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  helloworld
+//
+//  Created by Jeremy on 2020/9/24.
+//  Copyright © 2020 Jeremy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
