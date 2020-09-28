f39c8ac0bf5d7ce8c2dbaf57ddb6f15c20cddb40
sneakyowl42 <mathis.feron42@gmail.com>
2020-09-28 02:27:19 +0200

    main.cpp maj

--- main.cpp
+++ main.cpp
@@ -1,3 +1,19 @@
 #include <iostream>
-#include "MaClasse.hpp"
+#include <fstream>
 using namespace std;
+
+#include "Server.hpp"
+
+int main()
+{
+  std::ofstream toFile("output.txt");
+  Server A();
+
+  //ecriture console
+  std::cout<<"Server A: "<<A<<"\n";
+
+  //ecriture fichier
+  toFile<<"Server A: "<<A<<"\n";
+
+  return 0;
+}
