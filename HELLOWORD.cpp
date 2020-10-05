fbade3fe1ee71118db057477ff5534a68dedb042
m-aspro <71456033+m-aspro@users.noreply.github.com>
2020-10-05 21:56:20 +0200

    Add files via upload

---
+++ HELLOWORD.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello word" << endl;
+	return 0;
+}
+
+//pour compiler le code : g++ -o PremierProgramme PremierProgramme.cpp
+//pour exécuter le code : ./PremierProgramme
+
+//pour renommer le fichier (l'exécutable) :
+//commande : rename 'HELLOWORD' PremierProgramme.cpp
+
+//pour afficher les warning, il suffit d'exécuter le code
+//si des warning existent alors ils s'affichent 
