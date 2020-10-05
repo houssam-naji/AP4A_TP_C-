c99aa67ed62887521351b233c0871f29d498a477
louischdt <55501714+louischdt@users.noreply.github.com>
2020-10-05 15:40:53 +0200

    Surcharge + class server

---
+++ Server.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "Server.h"
+
+using namespace std;
+
+// Constructeur par défaut :
+
+Server::Server()
+{
+    bool consolActivation=true;
+    bool logActivation=true;
+}
+
+// Constructeur :
+
+Server::Server(bool consolA, bool logA)
+{
+    bool consolActivation=consolA;
+    bool logActivation=logA;
+}
+
+// Constructeur par recopie :
+
+Server::Server(const Server& server)
+{
+    bool consolActivation=server.consolActivation;
+    bool logActivation=server.logActivation;
+}
+
+//Destructeur :
+
+Server::~Server()
+{
+}
+
+void Server::consoleWrite(ostream &flux)
+{
+}
+
+void Server::fileWrite(ofstream &flux){
+}
+
+ofstream operator<<(ofstream &flux, Server const& server){
+    this->server.fileWrite(flux);
+    flux << endl;
+    return flux;
+}
+
+ostream operator<<(ostream &flux, Server const& server){
+  server.consoleWrite(flux);
+  flux << endl;
+  return flux;
+}
+
+Server& Server::operator=(Server const& server){
+    consolActivation=server.consolActivation;
+    logActivation=server.logActivation;
+}
---
+++ Server.h
@@ -0,0 +1,28 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+class Server
+{
+    public :
+        Server();
+        Server(bool consolActivation, bool logActivation);
+        Server(const Server&);
+        ~Server();
+        Server& operator=(const Server& server);
+        ostream& operator<<(ostream &flux);
+        ofstream& operator<<(ofstream &flux);
+        void consoleWrite(ostream &flux);
+        void fileWrite(ofstream &flux);
+
+    private :
+        bool consolActivation, logActivation;
+};
+
+
+#endif
