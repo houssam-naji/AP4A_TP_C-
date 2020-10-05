213807cae213bcca7acf35aa4421cabe5be2a527
Raphael Ribeiro <raphael.ribeiro.pro@gmail.com>
2020-10-05 23:46:45 +0200

    Adding Doxygen comments to Server.hpp

--- ReadMe.md
+++ ReadMe.md
@@ -1,3 +1,9 @@
 AP4A_TP_C++
 
-To compile just "make" 
+Pour compiler utiliser l'outil make.
+(Pour l'instant uniquement compatible avec linux)
+
+Les répertoires :
+
+bin/ <- contient l'executable final
+bon-int/ <- contient tous les fichiers intermédiares comme les .o
--- src/Server.hpp
+++ src/Server.hpp
@@ -19,13 +19,50 @@
 class Server
 {
 public:
+    /**
+     * @brief Constructeur de la class Serveur par defaut
+     */
     Server();
+    
+    /**
+     * @brief Constructeur de la class Serveur avec initialisation des variables de class
+     * @param consoleActivation − Bool permettant d'activer l'affichage dans la console
+     * @param logActivation − Bool permettant d'activer l'affichage dans un fichier
+     */
     Server(bool consoleActivation, bool logActivation);
+    
+    /**
+     * @brief Constructeur par recopie de la class Server
+     * @param otherServer − Instance de la class Server a copier
+     */
     Server(const Server& otherServer);
+    
+    /**
+     * @brief Surcharge de l'operateur d'affectation
+     * @return void
+     * @param otherServer − Reference d'une instance de la class Server constante
+     */
     void operator=(const Server& otherServer);
+    
+    /**
+     * @brief Destructeur de la class Server
+     */
     virtual ~Server();
 
+    /**
+     * @brief Fonction amie et surcharge de l'operateur << permettant l'affichage dans la console
+     * @return Reference de ostream de la std
+     * @param os − Reference ostream passe par defaut lorsque l'operateur << est appelle
+     * @param server − Instance de la class Server qui transmet les informations a afficher
+     */
     friend std::ostream& operator<<(std::ostream& os, const Server& server);
+    
+    /**
+     * @brief Fonction amie et surcharge de l'operateur << permettant l'affichage dans la console
+     * @return Reference de ofstream de la std
+     * @param of − Reference ofstream passe par defaut lorsque l'operateur << est appelle
+     * @param server − Instance de la class Server qui transmet les informations a afficher
+     */
     friend std::ofstream& operator<<(std::ofstream& of, const Server& server);
 
 private:
